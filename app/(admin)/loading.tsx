e5e6c082de5c44017ba4d991eec3f39806dfc4f5
Darius <dpdarius1@gmail.com>
2024-07-30 13:15:16 +0300

    adding loading fyle to the admin route

---
+++ app/(admin)/loading.tsx
@@ -0,0 +1,11 @@
+import Avatar from "@/components/Avatar";
+
+function Loading() {
+  return (
+    <div className="mx-auto animate-spin p-10">
+      <Avatar seed="Assistly icon" />
+    </div>
+  );
+}
+
+export default Loading;
